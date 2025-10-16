de6214e89b5389b8fae8bf0cd402adc70aac1779
elae <elahehoseini99626@gmail.com>
2025-10-16 17:46:42 -0300

    add new persian font  and make project as RTL

---
+++ src/app/test.js
@@ -0,0 +1,26 @@
+export default function testfunction (){
+    return (
+         <p>
+     الهه حسینی 
+    </p>
+    );   
+ 
+
+}
+
+export const testfunction2 =() =>{
+   return (
+         <p>
+     الهه حسینی 
+    </p>
+    );   
+ 
+}
+export const testfunction3 =() =>{
+  return (
+         <p>
+     الهه حسینی 
+    </p>
+    );   
+ 
+}
